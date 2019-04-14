19913e0c6c8c404943f073433eb45b2ae215b89d
Vagelis Giannakosian <e.giannakosian@gmail.com>
2019-04-14 14:33:48 +0300

    application completed!

--- starter/app.js
+++ starter/app.js
@@ -8,31 +8,90 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer;
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+var scores, roundScore, activePlayer, gamePlaying;
 
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
-document.getElementById('current-0').textContent = 0;
-document.getElementById('current-1').textContent = 0;
+init();
 
 
+//roll dice event
 document.querySelector('.btn-roll').addEventListener('click',function(){
-    //1. Random number
-   var dice = Math.floor(Math.random()*6)+1;
+    if(gamePlaying){
+        //1. Random number
+        var dice = Math.floor(Math.random()*6)+1;
 
-    //2. Display the result
-    var diceDOM = document.querySelector('.dice') ;
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-'+dice+'.png';
+        //2. Display the result
+        var diceDOM = document.querySelector('.dice') ;
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-'+dice+'.png';
 
-    //3. Update the roundscore if the rolled number was not 1
-
+        //3. Update the roundscore if the rolled number was not 1
+        if(dice !== 1){
+            //Add score
+            roundScore += dice;
+            document.querySelector('#current-'+activePlayer).textContent = roundScore;
+        }else{
+            //next player
+            nextPlayer();
+        }
+    }
 
 });
 
+//hold button event
+document.getElementsByClassName('btn-hold')[0].addEventListener('click',function(){
+    if(gamePlaying) {
+        //add the current score to the players global score
+        scores[activePlayer] += roundScore;
 
-//document.querySelector('#current-'+activePlayer).textContent = dice;
+        // update the UI
+        document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+        // check if the player won the game
+        if (scores[activePlayer] >= 20) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+            document.querySelector('.dice').style.display = 'none';
+            let winner = document.querySelector('.player-' + activePlayer + '-panel');
+            winner.classList.add('winner');
+            winner.classList.remove('active');
+            gamePlaying = false;
+
+        } else {
+            //next player
+            nextPlayer();
+        }
+    }
+});
+
+//new game button event
+document.getElementsByClassName('btn-new')[0].addEventListener('click',init);
+
+
+
+function nextPlayer(){
+    roundScore = 0;
+    document.getElementById('current-'+activePlayer).textContent = roundScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
+}
+function init() {
+
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    gamePlaying = true;
+//Set to 0 scores displayed in html
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
